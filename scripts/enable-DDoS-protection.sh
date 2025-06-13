a24f62b0b8254d5ee3187be372e8869775134230
chadmcrowell <chadmcrowell@gmail.com>
2025-06-13 13:52:39 +0200

    updated ddos script

---
+++ scripts/enable-DDoS-protection.sh
@@ -0,0 +1,39 @@
+# resource group
+export RESOURCE_GROUP_NAME=secure-rg
+
+# Create the DDoS Protection Plan
+az network ddos-protection create \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --name ddosForDefender \
+  --location eastus
+
+# get vnet name
+VNET_NAME=$(az network vnet list \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --query "[0].name" \
+  --output tsv)
+
+# get subscription ID
+export SUB_ID=$(az account show --query id --output tsv)
+
+# Associate plan with a VNET
+az network vnet update \
+  --name $VNET_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --ddos-protection-plan /subscriptions/$SUB_ID/resourceGroups/$RESOURCE_GROUP_NAME/providers/Microsoft.Network/ddosProtectionPlans/ddosForDefender
+
+
+# verify it's active
+az network vnet show \
+  --name $VNET_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --query "ddosProtectionPlan.id"
+  
+
+# remove DDoS protection
+az network vnet update \
+  --name $VNET_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --remove ddosProtectionPlan
+
+##
